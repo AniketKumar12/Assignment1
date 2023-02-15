bd305079104ac993537597c663f63db31a35bf49
Aniket Kumar <aniket84310152@gmail.com>
2023-02-15 13:22:04 +0530

    Added File

---
+++ assignment2/src/Assignment2/Product.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections.Generic;
+
+namespace Assignment
+{
+   public  class Product
+    {
+        string ProdutName{get;set;}
+        double ProdutPrice{get;set;}
+        int ProdutQuantity{get;set;}
+        string ProdutType{get;set;}
+        public string Name
+        {
+            get { return ProdutName; }
+            set { ProdutName = value; }
+        }
+        public double Price
+        {
+            get { return ProdutPrice; }
+            set { ProdutPrice = value; }
+        }
+        public int Quantity
+        {
+            get { return ProdutQuantity; }
+            set { ProdutQuantity = value; }
+        }
+
+        
+        
+        public string Type
+        {
+            get { return ProdutType; }
+            set { ProdutType = value; }
+        }
+        public override string ToString()
+        {
+            return "ProductName:-" + Name + " " +"ProductPrice:-"+ Price+" "+"ProdctQuantity:-"+Quantity+" "+"ProductType:-"+Type;
+        }
+       static List<Product> newlist = new List<Product>();
+
+
+        static void Main(string[] args)
+        {
+            List<Product> productList = new List<Product>() {
+                 new Product(){ Name="lettuce", Price=10.5 ,Quantity=50,Type="Leafy green"},
+                 new Product(){ Name="cabbage", Price=20 ,Quantity=100,Type="Cruciferous"},
+                 new Product(){ Name="pumpkin", Price=30,Quantity=30,Type="Marrow"},
+                 new Product(){ Name="cauliflower", Price=10 ,Quantity=25,Type="Cruciferous"},
+                 new Product(){ Name="zucchini", Price=20.5,Quantity=50,Type="Marrow"},
+                 new Product(){ Name="yam", Price=30,Quantity=50,Type="Root"},
+                 new Product(){ Name="spinach", Price=10 ,Quantity=100,Type="Leafy green"},
+                 new Product(){ Name="broccoli", Price=20.2,Quantity=75,Type="Cruciferous"},
+                 new Product(){ Name="Garlic", Price=30 ,Quantity=20,Type="Leafy green"},
+                 new Product(){ Name="silverbeet", Price=10 ,Quantity=50,Type="Marrow"},
+             };
+             Console.WriteLine("total number of Product in Product List: ",productList.Count);
+             foreach (Product item in productList)
+                 Console.WriteLine(item);
+
+          
+            Console.WriteLine("total number of Product List: ", newlist.Count);
+            Console.WriteLine("After Adding Product");
+            newlist.Add(new Product() { Name = "Potato", Price = 10, Quantity = 50, Type = "Root" } );
+            Console.WriteLine("total number of Product in Product List: ", newlist.Count);
+
+            foreach (var item in newlist)
+                Console.WriteLine(item);
+            foreach (var item in newlist)
+            {
+                if (item.Type.Equals("Leafy green"))
+                    Console.WriteLine(item);
+            }
+
+            var itemToRemove = newlist.Single(r => r.Name == "Garlic");
+            newlist.Remove(itemToRemove);
+            Console.WriteLine("total number of Product in Product List:-{0}", newlist.Count);
+
+            foreach (var item in newlist)
+            {
+                if (item.Name.Equals("cabbage"))
+                {
+                    int a = item.Quantity + 50 ;
+                    item.Quantity = a;
+                    Console.WriteLine(item);
+                }        
+            }
+            double total = 0;
+            foreach (var item in newlist)
+            {
+                if (item.Name.Equals("lettuce"))
+                {
+                    total = total + (1) * item.Price;
+                }
+                if (item.Name.Equals("zucchini,"))
+                {
+                    total = total + (2) * item.Price;
+                }
+                if (item.Name.Equals("broccoli"))
+                {
+                    total = total + (1) * item.Price;
+                }
+            }
+            Console.WriteLine("Rounded Value: ",total);
+        }
+    }
+}
